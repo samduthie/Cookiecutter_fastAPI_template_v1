d138d1527edb214aa4f33010529636748f98c6e2
Sam Duthie <samduthie@gmail.com>
2026-02-25 15:50:06 +0000

    Update github workflow

---
+++ {{cookiecutter.project_slug}}/.github/workflows/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Apply database migrations
+alembic upgrade head
+
+# Start the application using the command provided in docker-compose
+exec "$@"
